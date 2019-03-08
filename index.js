4fb215e0021477df8f82ea6ca3a4993fdc8ae8f3
github username <github email address>
2019-03-08 18:36:38 -0500

    Save

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+var commitResults = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
+function showCommits(repo) { repo =>
+  $.get(`https://api.github.com/repos/${repo.dataset.owner}/${repo.dataset.repository}/commits`, data => {
+    $('#details').html(commitResults(data))
+  })
+}
+
+let searchTemplate = (repo) => {
+  return `
+        <h2><a href="${repo.html_url}">${repo.name}</a></h2>
+        <p><a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${repo.description}</p>
+      <br>
+    `
+}
+
+let searchResults = (repos) => repos.items.map( repo => searchTemplate(repo))
+
+
+function searchRepositories() {
+  let term = document.getElementById("searchTerms").value
+  let url = 'https://api.github.com/search/repositories?q=' + term
+  $.get(url).done(function(data) {
+    $('#results').html(searchResults(data))
+  })
+}
+
 $(document).ready(function (){
 });
