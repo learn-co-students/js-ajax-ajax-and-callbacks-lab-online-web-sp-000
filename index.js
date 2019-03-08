9c482851032219012ba6d03d1fe32506d4f28ae4
Douglas Uretsky <douglasuretsky@gmail.com>
2019-03-08 17:05:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 $(document).ready(function (){
 });
+
+function displayError() {
+  return $('#errors').html("error");
+}
+
+function renderSearchResults(data) {
+  let repoList = data.items.map(repo => `<ul>
+                                          <li><strong>${repo.name}:</strong>
+                                            ${repo.description}
+                                            <a href="#" data-repo="${repo}"><button onclick="showCommits(data-repo)">Show Commits</button></a></li>
+                                          </ul>`)
+  return repoList
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $('#results').html(renderSearchResults(data))}).fail(error => {
+      displayError()
+    });
+}
+
+function showCommits(repo) {
+  $.get(`https://api.github.com/repos/${repo.dataset.owner}/${repo.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  });
+}
+
+function renderCommits(data) {
+}
